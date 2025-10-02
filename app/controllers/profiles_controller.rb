29e6e63d734c2e89e905ebcd41fc4986ad91943f
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jeremyarcafire@gmail.com>
2025-10-02 06:24:58 -0500

    Perfil nutricionista profesional + show/edit + estilos _profile.scss + navbar + estilo de navbar

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,27 @@
 class ProfilesController < ApplicationController
-  
-  def show
-  end
+  before_action :set_profile, only: [:show, :edit, :update]
 
-  def edit
+  def show; 
+  end
+  def edit;
   end
 
   def update
+    if @profile.update(profile_params)
+      redirect_to profile_path, notice: "Perfil actualizado correctamente."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_profile
+    # Carga el perfil del usuario logueado
+    @profile = current_user.profile || current_user.create_profile
+  end
+
+  def profile_params
+    params.require(:profile).permit(:weight, :height, :goals, :conditions, :lifestyle)
   end
 end
